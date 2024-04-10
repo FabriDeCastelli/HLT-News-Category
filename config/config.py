885eed78567d6e9867d05c152d58d71e792fb321
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-04-10 14:40:02 +0200

    Config file for project paths.

---
+++ config/config.py
@@ -0,0 +1,9 @@
+""" Configuration file for the project. """
+
+import os
+
+# PROJECT FOLDER PATH
+PROJECT_FOLDER_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# DATASET PATH
+DATASET_PATH = PROJECT_FOLDER_PATH + "/dataset/News_Category_Dataset.json"
