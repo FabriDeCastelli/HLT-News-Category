284a45af5773d73961dfdd18a85a4a5d29f214c4
francealip <f.aliprandi@studenti.unipi.it>
2024-04-11 12:31:29 +0200

    read and clean the dataset

--- config/config.py
+++ config/config.py
@@ -7,3 +7,12 @@ PROJECT_FOLDER_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 
 # DATASET PATH
 DATASET_PATH = PROJECT_FOLDER_PATH + "/dataset/News_Category_Dataset.json"
+
+#RENAME CATEGORIES
+life = ["WELLNESS", "TRAVEL", "STYLE & BEAUTY", "FOOD & DRINK"]
+entertainment = ["ENTERTAINMENT", "COMEDY"]
+voices = ["LATINO VOICES", "BLACK VOICES", "QUEER VOICES"]
+sports = ["SPORTS"]
+politics = ["POLITICS"]
+new_names = ["Life", "Entertainment", "Voices", "Sports","Politics"]
+drop_column = ["link","authors","date"]
--- src/main/utilities/utils.py
+++ src/main/utilities/utils.py
@@ -1,9 +1,18 @@
 """ This module contains utility functions for the project. """
 
 import pandas as pd
-from config.config import DATASET_PATH
+from config.config import *
 
 
+def label_ranaming(df):
+    di = {}
+    for new,orig in zip(new_names, [life, entertainment, voices, sports, politics]):
+        for label in orig:
+            di[label] = new
+    df = df.replace({"category": di})    
+    
+    return df[df['category'].isin(new_names)]
+        
 def get_dataset(filepath=DATASET_PATH):
     """
     Read the dataset from the given filepath and return the dataframe.
@@ -11,4 +20,7 @@ def get_dataset(filepath=DATASET_PATH):
     :param filepath: The path to the dataset file.
     :return: The dataframe containing the dataset.
     """
-    return pd.read_json(filepath, lines=True)
+    df = pd.read_json(filepath, lines=True)
+    df = label_ranaming(df) 
+    df.drop(labels=drop_column, inplace=True, axis=1)
+    return df
