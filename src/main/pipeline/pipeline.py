211f3ae0cc0622f9b20db1c9d0db72a14d9f2867
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-04-14 17:10:48 +0200

    Pipeline parallelism and automatic file saving

--- src/main/pipeline/pipeline.py
+++ src/main/pipeline/pipeline.py
@@ -1,7 +1,13 @@
 """ Pipeline module. """
 
+from config.config import LOGISTIC_PIPELINE_DATASET_PATH
+from datetime import datetime
 from functools import reduce
+from joblib import Parallel, delayed
 from typing import Callable, List
+
+import os
+import pandas as pd
 
 
 class Pipeline:
@@ -22,15 +28,46 @@ class Pipeline:
         """
         self.steps.append(step)
 
-    def execute(self, data):
+    def execute(self, data: pd.DataFrame, save=True):
         """
-        Run the pipeline.
+        Run the pipeline in parallel.
+        The pipeline is run in parallel by splitting the data into chunks and running
+        the pipeline on each chunk in parallel.
+        The number of chunks is equal to the number of cpu's available.
+        The results are flattened and returned.
+
+        :param data: The data to run the pipeline.
+        :return: The data after the processing.
         """
-        return list(
-            map(
-                lambda item: reduce(
-                    lambda previous, step: step(previous), self.steps, item
-                ),
-                data,
+
+        def execute_chuck(data):
+            """
+            Run the pipeline.
+            """
+            return list(
+                map(
+                    lambda item: reduce(
+                        lambda previous, step: step(previous), self.steps, item
+                    ),
+                    data,
+                )
             )
-        )
+
+        cpu_count = os.cpu_count()
+        results = {}
+
+        print("Running pipeline in parallel: number of cpu's: ", cpu_count)
+        for column in data.columns:
+            chunks = [data[column][i::cpu_count] for i in range(cpu_count)]
+            now = datetime.now()
+            results[column] = Parallel(n_jobs=cpu_count)(
+                delayed(execute_chuck)(chunk) for chunk in chunks
+            )
+            results[column] = [result for chunk in results[column] for result in chunk]
+            print("Time taken: ", datetime.now() - now)
+
+        print(results)
+        df = pd.DataFrame(results)
+        if save:
+            df.to_csv(LOGISTIC_PIPELINE_DATASET_PATH, index=False)
+        return df
